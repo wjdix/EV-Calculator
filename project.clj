54be36095b6b291a53f70e7ec77cf591177eabc0
William Dix <william.dix@getbraintree.com>
2012-10-28 20:09:07 -0700

    update lein version

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [enlive "1.0.0"]
